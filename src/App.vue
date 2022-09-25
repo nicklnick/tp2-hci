f93db99a9690ad2cf082dd44142bb0d35f8bf16b
Nicolás Margenat <nmargenat@itba.edu.ar>
2022-09-25 16:29:57 -0300

    feat: added top-bar
    Added top bar.
    TODO:
        - Custom logo inside button
        - Vertically center search bar
        - Put magnifying glass icon on search bar

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,50 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary">
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld />
+      <v-toolbar color="secondary">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-btn id="home-button" color="tertiary" raised large> </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-autocomplete
+          id="search-bar"
+          class="search-bar"
+          label="Search"
+          background-color="quaternary"
+          item-color="black"
+          dense
+          filled
+          rounded
+          solo-inverted
+        ></v-autocomplete>
+
+        <v-spacer></v-spacer>
+
+        <v-btn id="profile" color="tertiary" fab small>
+          <v-icon>account_circle</v-icon>
+        </v-btn>
+      </v-toolbar>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 
   data: () => ({
     //
   }),
 };
 </script>
+
+<style>
+.search-bar {
+  width: 35%;
+  display: flex;
+  align-self: baseline;
+}
+</style>
