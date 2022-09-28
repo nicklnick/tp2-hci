3741636b8da0daaa10f43b9db9f8c9bc832f62fd
Nicolás Margenat <nmargenat@itba.edu.ar>
2022-09-28 15:54:55 -0300

    style: separated CreateRoutinePanel for better modularization

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,19 @@
 <template>
   <v-app>
     <v-main>
-      <v-card id="create-routine" class="create-routine" color="quaternary" raised>
-        <v-flex class="d-flex centered-input">
-          <v-card class="flex-grow-1" color="secondary" outlined>Warmup</v-card>
-
-          <!-- TODO: vincular el for a la cantidad de Add Series que haya tocado la persona -->
-          <v-card v-for="n in 3" :key="n" class="flex-grow-1" color="secondary" outlined>Series {{n}}</v-card>
-
-          <v-card width="23%" color="secondary" outlined>Cooldown</v-card>
-        </v-flex>
-
-        <v-btn color="black--text primary" rounded>+ Add Routine</v-btn>
-
-      </v-card>
+      <CreateRoutinePanel></CreateRoutinePanel>
 
     </v-main>
   </v-app>
 </template>
 <script>
 
+import CreateRoutinePanel from "@/components/CreateRoutinePanel";
 export default {
   name: "App",
 
   components: {
+    CreateRoutinePanel
   },
   data: () => ({
     //
@@ -32,12 +22,4 @@ export default {
 </script>
 
 <style>
-.create-routine {
-  height: 46%;
-  width: 37%;
-}
-
-.centered-input {
-  text-align: center;
-}
 </style>
