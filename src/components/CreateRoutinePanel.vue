be6df8e8d099ab29326aaebe2bdff199cc934887
Nicolás Margenat <nmargenat@itba.edu.ar>
2022-09-29 17:46:49 -0300

    feat: progress on create-routine panel

--- src/components/CreateRoutinePanel.vue
+++ src/components/CreateRoutinePanel.vue
@@ -1,22 +1,47 @@
 <template>
-  <v-card id="create-routine" class="create-routine" color="quaternary" raised>
-    <v-flex class="d-flex centered-input">
-      <v-card class="flex-grow-1" color="secondary" outlined>Warmup</v-card>
+  <v-card id="create-routine" class="create-routine overflow-y-auto" color="quaternary" raised
+              max-height="400px">
+    <v-flex class="flex-column">
+      <v-card >
+        <!-- TODO: vincular el for a la cantidad de Add Series que haya tocado la persona -->
+        <v-tabs
+          class="rounded-t-lg"
+          background-color="secondary"
+          show-arrows
+        >
+          <v-tabs-slider color="tertiary"></v-tabs-slider>
 
-      <!-- TODO: vincular el for a la cantidad de Add Series que haya tocado la persona -->
-      <v-card v-for="n in 3" :key="n" class="flex-grow-1" color="secondary" outlined>Series {{n}}</v-card>
+          <v-tab
+            v-for="i in 10"
+            :key="i"
+            :href="'#tab-' + i"
+          >
+            Item {{ i }}
+          </v-tab>
+        </v-tabs>
+      </v-card>
 
-      <v-card width="23%" color="secondary" outlined>Cooldown</v-card>
+      <v-text-field v-for="n in 3" :key="n" class="routine-text" prepend-icon="drag_indicator"></v-text-field>
+
     </v-flex>
 
-    <v-btn color="black--text primary" rounded>+ Add Routine</v-btn>
-
+    <div class="floating-buttons">
+      <AddButton content="+ add exercise"></AddButton>
+      <AddButton content="+ add rest"></AddButton>
+    </div>
   </v-card>
 </template>
 
 <script>
+import AddButton from "@/components/AddButton";
 export default {
-  name: "CreateRoutinePanel"
+  data() {
+    return  {
+      toggle_exclusive: 2,
+    }
+  },
+  name: "CreateRoutinePanel",
+  components: { AddButton }
 };
 </script>
 
@@ -26,7 +51,17 @@ export default {
   width: 37%;
 }
 
+.routine-text {
+  width: 85%;
+}
+
 .centered-input {
   text-align: center;
+}
+
+.floating-buttons {
+  flex-direction: row;
+  display: flex;
+  justify-content: end;
 }
 </style>
