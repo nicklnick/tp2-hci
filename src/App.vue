ecdc312b78a141c5c842b81a0ae4c532a5313d6e
mafaldcosta <mafaldacpmc@gmail.com>
2022-09-30 11:07:54 -0300

    feat: users prof not finished

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
   <v-app>
-    <v-main class="pa-5">
-      <v-row>
-        <BigButton button_text="Create New Routine"></BigButton>
-        <ButtonWithIcon button_text="Arms"
-                        button_icon="muscles/muscle.png"></ButtonWithIcon>
-        <RoutineButton routine_author="Saul" routine_name="Core Strength"
-        routine_stars="4.75"></RoutineButton>
-      </v-row>
-    </v-main>
+    <v-spacer></v-spacer>
+    <div class="d-flex align-center justify-center">
+      <v-btn id="profile" color="tertiary" fab x-large>
+        <v-icon x-large>account_circle</v-icon>
+      </v-btn>
+    </div>
+    <v-spacer></v-spacer>
+    <div>
+      <p class="font-weight-bold">username</p>
+    </div>
+    <div>
+      <v-card class="d-flex flex-column align-center justify-center" flat tile>
+        <v-container class="pa-1">
+          <v-card
+            class="d-flex text-center justify-center align-center"
+            rounded-lg
+            color="tertiary"
+            height="35px"
+          >
+            Full Name
+          </v-card>
+          <v-card class="text-center my-3 justify-center align-center" roundend-lg color="tertiary" height="35px">
+            Birthday Date
+          </v-card>
+          <v-card class="text-center justify-center align-center" roundend-lg color="tertiary" height="35px">
+            E-mail
+            <v-btn icon color="black" small>
+              <v-icon>edit</v-icon>
+            </v-btn>
+          </v-card>
+        </v-container>
+      </v-card>
+    </div>
+    <v-spacer></v-spacer>
   </v-app>
 </template>
+
 <script>
-
-import BigButton from "@/components/BigButton";
-import ButtonWithIcon from "@/components/ButtonWithIcon";
-import RoutineButton from "@/components/RoutineButton";
-
 export default {
   name: "App",
 
-  components: {
-    BigButton,
-    ButtonWithIcon,
-    RoutineButton,
-  },
+  components: {},
+
   data: () => ({
     //
   }),
@@ -32,8 +50,11 @@ export default {
 </script>
 
 <style>
-.search-bar {
-  width: 35%;
-  align-self: center;
+.pa-1 {
+  width: 50%;
+}
+
+.font-weight-bold {
+  text-align: center;
 }
 </style>
