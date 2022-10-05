de3da594a24e320762683c92f00e20651a4ee8df
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2022-10-05 09:36:36 -0300

    feat: side Menu and Log In component

---
+++ src/components/LogIn Page/LogIn2.vue
@@ -0,0 +1,146 @@
+<template>
+  <v-card class="login">
+    <v-tabs v-model="tab" align-with-title color="secondary">
+      <v-tabs-slider color="secondary"></v-tabs-slider>
+      <v-tab class="section"> Log In </v-tab>
+      <v-tab-item>
+        <div class="details">
+          <v-form>
+            <v-container>
+              <v-col align="center">
+                <v-text-field
+                  v-model="email"
+                  :rules="[rules.required, rules.email]"
+                  label="Email"
+                  outlined
+                  color="secondary"
+                ></v-text-field>
+
+                <v-text-field
+                  v-model="password"
+                  :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                  :rules="[rules.required, rules.min]"
+                  :type="show1 ? 'text' : 'password'"
+                  name="input-10-1"
+                  hint="At least 8 characters"
+                  counter
+                  @click:append="show1 = !show1"
+                  label="Password"
+                  outlined
+                  color="secondary"
+                ></v-text-field>
+                <v-btn class="button" color="secondary"> Continuar </v-btn>
+              </v-col>
+            </v-container>
+          </v-form>
+        </div>
+      </v-tab-item>
+      <v-tab class="section">Sign Up</v-tab>
+      <v-tab-item>
+        <div class="details">
+          <v-form class="prueba2">
+            <v-container>
+              <v-col align="center">
+                <v-text-field
+                  v-model="email"
+                  :rules="[rules.required, rules.email]"
+                  label="Email"
+                  outlined
+                  color="secondary"
+                ></v-text-field>
+
+                <v-text-field
+                  v-model="first"
+                  label="Username"
+                  outlined
+                  color="secondary"
+                ></v-text-field>
+
+                <v-text-field
+                  v-model="password"
+                  label="Password"
+                  :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                  :rules="[rules.required, rules.min]"
+                  :type="show1 ? 'text' : 'password'"
+                  name="password"
+                  hint="At least 8 characters"
+                  counter
+                  @click:append="show1 = !show1"
+                  outlined
+                  color="secondary"
+                ></v-text-field>
+
+                <v-text-field
+                  v-model="ConfirmPassword"
+                  name="confirmpassword"
+                  :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                  :rules="[
+                    rules.required,
+                    !!ConfirmPassword || 'type confirm password',
+                    password === ConfirmPassword ||
+                      'The password confirmation does not match.',
+                  ]"
+                  :type="show1 ? 'text' : 'password'"
+                  hint="Same password"
+                  counter
+                  @click:append="show1 = !show1"
+                  label="Re Enter Password"
+                  outlined
+                  color="secondary"
+                ></v-text-field>
+                <v-btn class="button prueba2" color="secondary">
+                  Continuar
+                </v-btn>
+              </v-col>
+            </v-container>
+          </v-form>
+        </div>
+      </v-tab-item>
+    </v-tabs>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "LogIn2",
+  data() {
+    return {
+      show1: false,
+      title: "Preliminary report",
+      email: "",
+      password: "",
+      ConfirmPassword: "",
+      rules: {
+        required: (value) => !!value || "Required.",
+        confirmPasswordRules: [
+          (value) => !!value || "type confirm password",
+          (value) =>
+            value === this.password ||
+            "The password confirmation does not match.",
+        ],
+        min: (v) => v.length >= 8 || "Min 8 characters",
+        email: (value) => {
+          const pattern =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Invalid e-mail.";
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style>
+.login {
+  width: 50%;
+}
+
+.section {
+  width: 50%;
+  color: rebeccapurple;
+}
+
+.details {
+  height: 75vh;
+}
+</style>
---
+++ src/components/sideMenu.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-card class="navDrawer" height="100%" width="20%">
+    <v-navigation-drawer permanent color="primary">
+      <v-list class="navbar" dense nav height="70%">
+        <v-list-item-group class="button" v-model="selectedItem">
+          <v-list-item v-for="item in items" :key="item.title" link>
+            <v-list-item-icon v-if="item.icon">
+              <v-icon color="#000000">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+
+      <v-list dense nav>
+        <v-list-item
+          height="auto"
+          class="align-content-end"
+          v-for="item in items2"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon color="#000000">{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "SideMenu",
+  data() {
+    return {
+      items: [
+        { title: "Home", icon: "mdi-home" },
+        { title: "Routines", icon: "mdi-book" },
+        { title: "Favourites", icon: "mdi-heart" },
+      ],
+      items2: [
+        { title: "Settings", icon: "mdi-cog" },
+        { title: "Help", icon: "mdi-help-circle" },
+        { title: "Log Out", icon: "mdi-logout" },
+      ],
+      right: null,
+    };
+  },
+};
+</script>
+
+<style>
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  width: 100%;
+}
+
+.button {
+  width: 100%;
+}
+
+.navDrawer {
+  display: inline;
+  float: left;
+}
+</style>
