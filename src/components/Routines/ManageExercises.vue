f9917b25edd2167e3c760c7433062d83ccd7bd4f
MSGronda <mgronda@itba.edu.ar>
2022-10-05 16:44:02 -0300

    refactor: reworking of manage exercises [WIP]

--- src/components/Routines/ManageExercises.vue
+++ src/components/Routines/ManageExercises.vue
@@ -10,6 +10,31 @@
 
       <div class="cwidth row-center overflow-auto">
         <div class="general-area-width">
+          <!-- Create new exercise -->
+
+          <v-card raised class="new-activity" v-if="mode === 1" color="quaternary" >
+            <div>
+              <h3 >New exercise</h3>
+              <v-text-field rounded outlined color="secondary"  hide-details
+                            background-color="tertiary" label="Exercise name"
+                            class="py-2"></v-text-field>
+              <v-text-field rounded outlined color="secondary"  hide-details
+                            background-color="tertiary" label="Exercise length (s)"
+                            class="py-2"></v-text-field>
+              <v-text-field rounded outlined color="secondary"  hide-details
+                            background-color="tertiary" label="Number of series"
+                            class="py-2"></v-text-field>
+              <div class="end-row">
+                  <v-btn fab color="secondary"
+                  @click="changeMode">
+                    <v-icon>check</v-icon>
+                  </v-btn>
+              </div>
+            </div>
+
+          </v-card>
+
+          <!-- View existing exercises -->
           <v-card color="d-flex justify-space-between my-2 quaternary" v-for="(activity, index) in activities"
                   :key="index">
             <div class="pl-3 pt-1">
@@ -20,14 +45,14 @@
             <v-btn text fab x-small color="dark-grey"
                    @click="removeActivity(index)"
             ><v-icon>mdi-close</v-icon></v-btn>
-
           </v-card>
+
         </div>
       </div>
 
 
       <v-btn class="mb-10" fab absolute right bottom color="secondary"
-      @click="addActivity('New Activity')"
+      @click="changeMode()"
       >
         <v-icon>mdi-plus</v-icon>
       </v-btn>
@@ -47,7 +72,8 @@ export default {
   name: "ManageExercises",
 
   data: () => ({
-    activities: [ new Activity("chau")]
+    mode: 0,
+    activities: [ new Activity("chau"),new Activity("chau"), new Activity("chau"), new Activity("chau"), new Activity("chau"), new Activity("chau"), new Activity("chau"),  ]
   }),
   methods: {
     removeActivity(index){
@@ -55,6 +81,9 @@ export default {
     },
     addActivity(name) {
       this.activities.push(new Activity(name));
+    },
+    changeMode() {
+      this.mode = this.mode === 0 ? 1 : 0;
     }
   },
 };
@@ -66,10 +95,10 @@ export default {
 }
 
 .cwidth {
-  width: 400px;
+  width: 800px;
 }
 .cheight {
-  height: 500px;
+  height: 600px;
 }
 .row-center {                 /* !!!! COMMON !!!! */
   display: flex;
@@ -84,7 +113,10 @@ export default {
   justify-content: start;
   flex-direction: column;
 }
-
+.new-activity{
+  margin-bottom: 20px;
+  margin-top: 20px;
+}
 
 .general-area-width {                /* !!!! COMMON !!!! */
   width: 90%;
@@ -94,5 +126,10 @@ export default {
    justify-content: end;
    flex-direction: row;
  }
+.row-start {
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+}
 
 </style>
