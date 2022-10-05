6c081c9b03c346606682d376b92a61001efce7e5
MSGronda <mgronda@itba.edu.ar>
2022-10-05 15:04:57 -0300

    feat: added testing view.
    To access: http://localhost:8080/#/test
    EVENTUALLY REMOVE!!!

---
+++ src/views/TestView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="general-area">
+
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "TestView",
+  components: {
+
+},
+};
+</script>
+
+<style scoped>
+.general-area{               /* !!!! COMMON !!!! */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+
+
+</style>
