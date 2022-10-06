ba1fd6ebc039529e22292cb62c8094963a6156f8
MSGronda <mgronda@itba.edu.ar>
2022-10-06 14:24:40 -0300

    fix: removed top nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
     <v-app>
-      <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-      </nav>
       <router-view/>
     </v-app>
 
