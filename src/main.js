1e1e390580c3dba3a3d1ae5bfab0f1f8069f7f55
MSGronda <mgronda@itba.edu.ar>
2022-10-08 16:22:44 -0300

    feat: added pinia

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from './router'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+import VueCompositionAPI from '@vue/composition-api'
+
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
+Vue.use(VueCompositionAPI)
 
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   router,
+  pinia,
   render: (h) => h(App)
 }).$mount("#app");
