f35374b40b046ab80fdea13e8175212192304d7e
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2022-10-12 19:44:52 -0300

    feat: 404 page

---
+++ src/views/LostView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="screen">
+    <div>
+      <h1>Uh Oh</h1>
+      <h1>Where did you go?</h1>
+      <h1>(._.) ( l: ) ( .-. ) ( :l ) (._.)</h1>
+    </div>
+    <router-link to="/home" tag="button">
+      <v-btn color="secondary">Go Home</v-btn>
+    </router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'LostView'
+}
+</script>
+
+<style scoped>
+.screen {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-content: center;
+  align-items: center;
+  justify-items: center;
+  padding-top: 50px;
+}
+
+</style>
