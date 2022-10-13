d43e9f758919001c9cfe645ee668ab4134d33080
Nicolás Margenat <nmargenat@itba.edu.ar>
2022-10-13 20:31:53 -0300

    refactor: modified routineButton

--- src/components/Routines/RoutineButton.vue
+++ src/components/Routines/RoutineButton.vue
@@ -1,61 +1,38 @@
 <template>
-
     <v-btn color="quaternary" height="200px" width="250px">
       <v-card color="quaternary" height="200px" width="250px">
+        <div class="flex-container">
+          <v-card-title> {{routine_name}} </v-card-title>
+          <v-card-subtitle>By: {{routine_author}}</v-card-subtitle>
+          <v-card-text class="text-start">
 
-        <div class="custom_card card_size">
-
-            <div>
-              <h3 class="card_text ml-4 pb-0 pt-3">
-                {{routine_name}}
-              </h3 >
-              <h5  class="card_text ml-4 pt-0">
-                By: {{routine_author}}
-              </h5>
-              <v-divider class="mx-4 mt-1 mb-4"></v-divider>
-              <h5 class="card_text ml-4 pt-0">Difficulty: {{routine_difficulty}}</h5>
-              <h5 class="details_text ml-4 mr-4 pt-0">Details: {{routine_detail}}</h5>
+            <div class="align-content-start justify-content-start">
+              {{routine_detail}}
             </div>
-
-            <div>
-<!--              <v-row class="ml-2">-->
-<!--                <v-card color="secondary" class="px-1 mx-1 text-caption rounded-pill"-->
-<!--                >arms</v-card>-->
-<!--                <v-card color="secondary" class="px-1 mx-1  text-caption rounded-pill"-->
-<!--                >chest</v-card>-->
-<!--                <v-card color="secondary" class="px-1 mx-1 text-caption rounded-pill"-->
-<!--                >glutes</v-card>-->
-<!--              </v-row>-->
-            </div>
-
-
-            <div class="justify-end">
-              <v-card-actions  class="py-0 justify-space-between">
-
-<!--                <div>-->
-<!--                  <v-icon small class="pr-2" color="primary">mdi-star</v-icon>-->
-<!--                  <span>{{routine_stars}}/5</span>-->
-<!--                </div>-->
-                <v-spacer></v-spacer>
-
-                <v-btn @click="switchColor()" icon >
-                  <v-icon :color="heart_color_comp"
-                  >mdi-heart</v-icon>
-                </v-btn>
-
-              </v-card-actions>
-            </div>
-
+          </v-card-text>
         </div>
+        <v-card-actions class="justify-end">
+          <v-chip-group>
+            <v-chip small color="tertiary">
+              {{routine_category}}
+            </v-chip>
+            <v-chip small color="tertiary">
+              {{routine_difficulty}}
+            </v-chip>
 
+          </v-chip-group>
+          <v-btn @click="switchColor()" icon>
+            <v-icon :color="heart_color_comp"
+            >mdi-heart</v-icon>
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </v-btn>
-
-
-
 </template>
 
 <script>
+import {useSecurityStore} from "@/stores/SecurityStore";
+
 export default {
   name: "RoutineButton",
   data: () => ({
@@ -79,6 +56,10 @@ export default {
         type: String,
         required: true
       },
+      routine_category: {
+        type: String,
+        required: true
+      }
   },
   computed: {
     heart_color_comp() {
@@ -90,11 +71,22 @@ export default {
       this.heart_color = this.heart_color === "primary" ? "white" : "primary";
     }
   },
+  async created() {
+    // Siempre inicializarlo!!!!!
+    const securityStore = useSecurityStore()
+    await securityStore.initialize()
+  }
 };
 </script>
 
 <!-- TODO: fix color!!! -->
 <style scoped>
+.flex-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+}
 
 .card_text {
   text-align: left;
