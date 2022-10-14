648469a5d562a1d6dad48eb2abf90125c0bc7657
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2022-10-14 10:53:32 -0300

    feat: correct behavior for favourite routines

--- src/components/Routines/RoutineButton.vue
+++ src/components/Routines/RoutineButton.vue
@@ -20,7 +20,7 @@
             </v-chip>
 
           </v-chip-group>
-          <v-btn @click="switchColor()" icon>
+          <v-btn @click="update()" icon v-model="favourite">
             <v-icon :color="heart_color_comp"
             >mdi-heart</v-icon>
           </v-btn>
@@ -31,12 +31,15 @@
 
 <script>
 import {useSecurityStore} from "@/stores/SecurityStore";
+import { useFavouriteStore} from "@/stores/FavouriteStore";
+import { FavouriteApi } from "@/api/favourites";
+import {mapActions} from "pinia";
 
 export default {
   name: "RoutineButton",
   data: () => ({
       heart_color: "white",
-
+      favourite: 0
   }),
   props:{
       routine_name: {
@@ -58,23 +61,52 @@ export default {
       routine_category: {
         type: String,
         required: true
+      },
+      routine_id: {
+        type: String,
+        required: true
       }
   },
   computed: {
     heart_color_comp() {
       return this.heart_color;
     },
+    ...mapActions(useFavouriteStore, {
+      $addToFavourites: 'addToFavourites',
+      $removeFromFavourites: 'remove',
+    })
   },
   methods: {
+    update(){
+      this.switchColor()
+      this.alternateFavourite()
+    },
     switchColor() {
       // TODO: Para ustedes ta bueno este blanco?
       this.heart_color = this.heart_color === "primary" ? "white" : "primary";
+    },
+    async alternateFavourite() {
+      if (parseInt(this.favourite) === 0) {
+        this.favourite = 1;
+        await FavouriteApi.markAsFavourite(parseInt(this.routine_id))
+        this.heart_color = "primary"
+      } else {
+        this.favourite = 0;
+        await FavouriteApi.removeAsFavourite(parseInt(this.routine_id))
+        this.heart_color = "white"
+      }
     }
   },
   async created() {
     // Siempre inicializarlo!!!!!
     const securityStore = useSecurityStore()
     await securityStore.initialize()
+
+    const Fav = await FavouriteApi.checkFavourite(parseInt(this.routine_id))
+    if( Fav === true){
+      this.favourite = 1
+      this.heart_color = "primary"
+    }
   }
 };
 </script>
