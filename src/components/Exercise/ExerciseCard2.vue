4b50839e71128f5eed84e664ceaddcec4d463cd7
Nicolás Margenat <nmargenat@itba.edu.ar>
2022-10-15 12:05:51 -0300

    feat: finished viewRoutine

--- src/components/Exercise/ExerciseCard2.vue
+++ src/components/Exercise/ExerciseCard2.vue
@@ -1,8 +1,8 @@
 <template>
   <v-card class="my-2 quaternary" >
-    <div class="d-flex justify-space-between ">
+    <div class="d-flex justify-space-between">
       <div  class="exercise-text-width col-start pl-3 pt-1">
-        <v-list-item-title class="align-self-start headline pb-3">{{exer_name}}</v-list-item-title>
+        <v-list-item-title class="align-self-start headline pb-3 title_text">{{exer_name}}</v-list-item-title>
         <v-divider  class="py-2"></v-divider>
         <v-list-item-subtitle class="pa-0 ma-0 exercise-text-width card_text">Details: {{exer_detail}}</v-list-item-subtitle>
         <v-list-item-subtitle class="pa-0 ma-0 exercise-text-width card_text">Type: {{exer_type[0].toUpperCase() + exer_type.substring(1)}}</v-list-item-subtitle>
@@ -37,7 +37,7 @@ export default {
 
 <style scoped>
 .exercise-text-width{
-  width: 85%;
+  width: 70%;
 }
 
 .card_text {
@@ -45,6 +45,11 @@ export default {
   text-overflow: ellipsis;
   white-space: nowrap;
   overflow: hidden;
+  max-width: 100%;
+}
+
+.title_text {
+  max-width: 90%;
 }
 
 </style>
--- src/views/OverviewRoutineView.vue
+++ src/views/OverviewRoutineView.vue
@@ -14,131 +14,137 @@
         <div class="space-between-col total-height py-10 width">
 
           <!-- General -->
-          <div class="space-between-row width px-10">
+          <div class="space-between-row px-10">
             <router-link class="button-link" to="/routines">
               <v-btn fab color="tertiary">
                 <img width="30" height="30" src="@/assets/icons/chevron-left.svg" alt="left">
               </v-btn>
             </router-link>
-            <h1>{{this.routine.routine_name}}</h1>
+            <div class="justify-self-center title_text">
+              <!-- conditional rendering -->
+              <h1>{{this.routine.routine_name}}</h1>
+            </div>
             <v-card></v-card>
           </div>
           <!-- /General -->
 
 
           <!-- Overview -->
-          <div class="row-center width">
-            <div class="pr-7 cwidth row-start">
-              <v-card id="main-panel" class="cwidth cheight rounded-xl" color="quaternary" raised>
-                <div class="cheight space-between-col">
+            <div  class="row-center twidth">
+              <div class="pr-7 cwidth row-start">
+                <v-card id="main-panel" class="cwidth cheight rounded-xl" color="quaternary" raised>
+                  <div class="cheight space-between-col">
 
-                  <div>
-                    <v-card class="cwidth rounded-b-0 rounded-t-xl row-start" color="secondary">
-                      <v-tabs class="tab-size-width rounded-tl-xl" background-color="secondary" show-arrows
-                              v-model="tab">
-                        <v-tabs-slider color="tertiary"></v-tabs-slider>
-                        <v-tab v-for="(cycle, index) in this.routine.cycles" :key="index">
-                          <div class="flex-row justify-space-between">
-                            {{cycle.cycle_name}}
+                    <div>
+                      <v-card class="cwidth rounded-b-0 rounded-t-xl row-start" color="secondary">
+                        <v-tabs class="tab-size-width rounded-tl-xl" background-color="secondary" show-arrows
+                                v-model="tab">
+                          <v-tabs-slider color="tertiary"></v-tabs-slider>
+                          <v-tab v-for="(cycle, index) in this.routine.cycles" :key="index">
+                            <div class="flex-row justify-space-between">
+                              {{cycle.cycle_name}}
+                            </div>
+                          </v-tab>
+                        </v-tabs>
+                      </v-card>
+                    </div>
+
+                    <!-- exercise card -->
+                    <div class="row-center overflow-auto">
+                      <div class="general-area-width general-area-height">
+                        <ExerciseCard2 class="my-2 quaternary"
+                                       v-for="(exercise, index) in this.routine.cycles[tab].exercises" :key="index"
+                                       :exer_name="exercise.exercise_name"
+                                       :exer_detail="exercise.exercise_detail"
+                                       :exer_type="exercise.exercise_type"
+                        >
+                          <!-- exercise time and series -->
+                          <div class="flex-container py-2 space-evenly-col">
+                            <div v-if="exercise.exericse_duration > 0">
+                              <v-text-field readonly v-model="exercise.exericse_duration" label="Time (s)"></v-text-field>
+                            </div>
+                            <div v-if="exercise.exercise_repetitions > 0">
+                              <v-text-field readonly v-model="exercise.exercise_repetitions" label="Repetitions"></v-text-field>
+                            </div>
                           </div>
-                        </v-tab>
-                      </v-tabs>
-                    </v-card>
-                  </div>
-
-                  <!-- exercise card -->
-                  <div class="row-center overflow-auto">
-                    <div class="general-area-width general-area-height">
-                      <ExerciseCard2 class="my-2 quaternary"
-                                     v-for="(exercise, index) in this.routine.cycles[tab].exercises" :key="index"
-                                     :exer_name="exercise.exercise_name"
-                                     :exer_detail="exercise.exercise_detail"
-                                     exer_type="Marcos:)"
-                      >
-                        <!-- exercise time and series -->
-                        <div class="pt-2 space-evenly-col">
-                          <v-text-field solo outlined filled rounded readonly :value="exercise.exericse_duration" background-color="tertiary"></v-text-field>
-                          <v-text-field solo outlined filled rounded readonly :value="exercise.exercise_repetitions" background-color="tertiary"></v-text-field>
-                        </div>
-                        <!-- /exercise time and series -->
-                      </ExerciseCard2>
-                    </div>
-                  </div>
-                  <!-- /exercise card -->
-
-                  <!-- repeat text -->
-                  <div class="row-center">
-                    <div class="general-area-width">
-                      <v-divider></v-divider>
-
-                      <div class="row-start">
-                        <p class="pl-3 pt-2 text-field">Repeat {{this.routine.cycles[tab].cycle_repetitions}} times</p>
+                          <!-- /exercise time and series -->
+                        </ExerciseCard2>
                       </div>
+                    </div>
+                    <!-- /exercise card -->
 
-                      <v-spacer></v-spacer>
+                    <!-- repeat text -->
+                    <div class="row-center">
+                      <div class="general-area-width">
+                        <v-divider></v-divider>
+
+                        <div class="row-start">
+                          <p class="pl-3 pt-2 text-field text-color">Repeat {{this.routine.cycles[tab].cycle_repetitions}} times</p>
+                        </div>
+
+                        <v-spacer></v-spacer>
+                      </div>
+                    </div>
+                  </div>
+                </v-card>
+              </div>
+              <!-- /repeat text -->
+
+              <!-- side panel (overview) -->
+              <CustomCard card-width="400" card-height="550" card-title="Overview">
+                <div class="general-area-height">
+                  <div class="overflow-auto px-4 mt-2">
+                  <v-textarea rounded outlined color="secondary"
+                              readonly
+                              background-color="tertiary" label="Description" no-resize
+                              class="py-2" :value="this.routine.routine_detail"
+                              ></v-textarea>
+                  </div>
+
+                  <div class="px-3">
+                    <v-divider class="mb-2"></v-divider>
+                    <div class="space-between-row">
+                      <h4>Category</h4>
+                      <v-chip-group>
+                        <v-chip color="tertiary">
+                          {{this.routine.routine_category.name.toUpperCase()}}
+                        </v-chip>
+                      </v-chip-group>
+                    </div>
+
+                    <v-divider class="my-2"></v-divider>
+
+                    <div class="space-between-row">
+                      <div>
+                        <h4>Difficulty: </h4>
+                      </div>
+                      <div>
+                        <v-chip :color="difficulty_color" class="font-weight-bold">
+                          {{this.routine.routine_difficulty.toUpperCase()}}
+                        </v-chip>
+                      </div>
+                    </div>
+                    <div class="mt-2 space-between-row">
+                      <h4>Public: </h4>
+                      <v-switch v-if="!this.routine" readonly></v-switch>
+                      <v-switch v-else readonly v-model="this.routine.routine_is_public" class="pa-0 ma-0" hide-details></v-switch>
                     </div>
                   </div>
                 </div>
-              </v-card>
+              </CustomCard>
+              <!-- /side panel (overview) -->
             </div>
-            <!-- /repeat text -->
-
-            <!-- side panel (overview) -->
-            <CustomCard card-width="400" card-height="550" card-title="Overview">
-              <div class="general-area-height">
-                <div class="overflow-auto px-4">
-                <v-textarea rounded outlined color="secondary"
-                            background-color="tertiary" label="Description" no-resize
-                            class="py-2" :value="this.routine.routine_detail"
-                            ></v-textarea>
-                </div>
-
-                <v-divider></v-divider>
-
-                <div class="space-between-row">
-                  <h4>Category</h4>
-                  <v-chip-group>
-                    <v-chip color="tertiary"
-                            v-for="(category, index) in this.routine.routine_category" :key="index">
-                      {{ category.name }}
-                    </v-chip>
-                  </v-chip-group>
-                </div>
-
-                <v-divider></v-divider>
-
-                <div class="space-between-row">
-                  <h4>Difficulty: </h4>
-                  <div>
-                    <v-card-actions class="py-0 my-0 justify-center">
-                      <v-select :items="this.difficulties"
-                                dense rounded hide-details single-line
-                                color="secondary" item-color="secondary"
-                                background-color="secondary"
-                                class="select_it"
-                      ></v-select>
-                    </v-card-actions>
-                  </div>
-                </div>
-                <div class="space-between-row">
-                  <h4>Public: </h4>
-                  <v-switch v-model="isPublic" class="pa-0 ma-0" hide-details></v-switch>
-                </div>
+            <!-- General -->
+            <div class="space-between-row width px-10">
+              <v-spacer></v-spacer>
+              <router-link to="/routines">
+                <v-btn fab x-large color="primary">
+                  <v-icon>edit</v-icon>
+                </v-btn>
+              </router-link>
               </div>
-            </CustomCard>
-            <!-- /side panel (overview) -->
-          </div>
-          <!-- General -->
-          <div class="space-between-row width px-10">
-            <v-spacer></v-spacer>
-            <router-link to="/routines">
-              <v-btn fab x-large color="primary">
-                <v-icon>edit</v-icon>
-              </v-btn>
-            </router-link>
-            </div>
 
-        </div>
+            </div>
         <!-- -->
 
       </div>
@@ -154,7 +160,7 @@
 import TopBar from "@/components/Navigation/TopBar";
 import SideMenu from "@/components/Navigation/SideMenu";
 import {useSecurityStore} from "@/stores/SecurityStore";
-import {CompleteRoutineApi} from "@/api/CompleteRoutineApi";
+import {CompleteRoutine, CompleteRoutineApi} from "@/api/CompleteRoutineApi";
 import {mapActions, mapState} from "pinia";
 import ExerciseCard2 from "@/components/Exercise/ExerciseCard2";
 import CustomCard from "@/components/CommonComponents/CustomCard";
@@ -169,10 +175,10 @@ export default {
   },
   data: () => {
     return {
-      routine: null,
+      routine: new CompleteRoutine("", "", false, "", ""),
       tab: 0,
-      difficulties: ["Rookie", "Beginner", "Intermediate", "Advanced", "Expert"],
-      isPublic: true
+      isPublic: true,
+      difficulty_color: null
     }
   },
   computed: {
@@ -190,6 +196,20 @@ export default {
       return this.routine.cycles[0];
     },
   },
+  methods: {
+    getDifficultyColor() {
+      if(this.routine.routine_difficulty === "rookie")
+        this.difficulty_color = "blue lighten-4"
+      else if(this.routine.routine_difficulty === "beginner")
+        this.difficulty_color = "blue lighten-1"
+      else if(this.routine.routine_difficulty === "intermediate")
+        this.difficulty_color =  "yellow"
+      else if(this.routine.routine_difficulty === "advanced")
+        this.difficulty_color = "orange"
+      else
+        this.difficulty_color = "red"
+    },
+  },
   async created() {
     const securityStore = useSecurityStore();
     await securityStore.initialize();
@@ -204,6 +224,7 @@ export default {
       await this.$router.push({name: "Login"});
     }
     this.routine = await CompleteRoutineApi.getCompleteRoutine(this.$route.params.id);
+    this.getDifficultyColor();
   }
 };
 </script>
@@ -230,7 +251,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  zoom: 90%;
+  zoom: 82%;
 }
 
 .text-width{
@@ -311,5 +332,21 @@ export default {
   bottom: 30px;
   right: 33%;
   width: 400px;
+}
+
+.flex-container {
+  display: flex;
+  width: 200px;
+}
+
+.title_text {
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  max-width: 50%;
+}
+
+.twidth {
+  width: 90%;
 }
 </style>
